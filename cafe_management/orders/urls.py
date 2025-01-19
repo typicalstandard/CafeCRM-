13e26a596690c314ddcd494bf8896843f4dadb30
dikay <dikiy@gmai.com>
2025-01-19 22:35:44 +0300

    Добавления обратной ссылки

--- cafe_management/orders/urls.py
+++ cafe_management/orders/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.OrderListView.as_view(), name='order_list'),
+    path('', views.OrderListView.as_view(), name='home'),
     path('orders/add/', views.OrderCreateView.as_view(), name='order_create'),
     path('orders/delete/', views.OrderDeleteFormView.as_view(), name='order_delete'),
     path('orders/search/',views.OrderSearchView.as_view(),name='order_search'),
