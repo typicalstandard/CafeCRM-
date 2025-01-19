e127062bc365d81466e4a8f16fa3090665d0f358
dikay <dikiy@gmai.com>
2025-01-19 21:58:30 +0300

    Перенес проверку в из clean в error_massages

--- cafe_management/orders/forms.py
+++ cafe_management/orders/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Order
 from menu.models import Dish
 
+from django.core.exceptions import ValidationError
+
 from .validators import validate_order_id
 
 
@@ -11,30 +13,23 @@ class OrderForm(forms.ModelForm):
         queryset=Dish.objects.all(),
         widget=forms.CheckboxSelectMultiple,
         label='Список блюд',
-        required=True
+        required=True,
+        error_messages={'required': 'Необходимо выбрать хотя бы одно блюдо.'}
     )
 
     class Meta:
         model = Order
         fields = ['table_number', 'items']
 
-    def clean_items(self):
-        items = self.cleaned_data.get('items')
-        if not items:
-            raise forms.ValidationError('Необходимо выбрать хотя бы одно блюдо.')
-        return items
 
 
 class OrderDeleteForm(forms.Form):
     order_id = forms.IntegerField(label='Введите ID заказа для удаления', required=True,validators=[validate_order_id])
 
-
-
-
-
 class OrderSearchForm(forms.Form):
     table_number = forms.IntegerField(required=False, label='Номер стола')
     status = forms.ChoiceField(choices=[
+        ('', '------'),
         ('waiting', 'в ожидании'),
         ('ready', 'готово'),
         ('paid', 'оплачено')
@@ -42,10 +37,11 @@ class OrderSearchForm(forms.Form):
 
 
 class OrderStatusForm(forms.Form):
-    order_id = forms.IntegerField(label='ID заказа',validators=[validate_order_id])
-    status = forms.ChoiceField(label='Статус заказа', choices=[
+    order_id = forms.IntegerField(label='ID заказа',required=True,validators=[validate_order_id])
+    status = forms.ChoiceField(label='Статус заказа',
+    choices=[
         ('waiting', 'в ожидании'),
         ('ready', 'готово'),
         ('paid', 'оплачено')
-    ])
+    ],required=True)
 
