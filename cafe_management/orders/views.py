a8febfeba8f9118964cc0eaffa3bfbf6949bf8a5
dikay <dikiy@gmai.com>
2025-01-19 21:16:49 +0300

    Фикс дублирования заказов

--- cafe_management/orders/views.py
+++ cafe_management/orders/views.py
@@ -8,13 +8,6 @@ class OrderCreateView(CreateView):
     template_name = 'orders_create.html'
     title_page = 'Добавление заказа'
 
-    def form_valid(self, form):
-        table_number = form.cleaned_data['table_number']
-        items = form.cleaned_data['items']
-        order = Order.objects.create(table_number=table_number)
-        order.items.set(items)
-        return super().form_valid(form)
-
 
 class OrderDeleteFormView(FormView):
     template_name = 'orders_delete.html'
