6e1ba4afba55c3f303aa182e9a5b27f7f722f91f
dikay <dikiy@gmai.com>
2025-01-20 04:16:48 +0300

    удаление заказа

--- cafe_management/api/urls.py
+++ cafe_management/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateOrderAPIView
+from .views import CreateOrderAPIView, DeleteOrderAPIView
 
 urlpatterns = [
     path('orders/create/', CreateOrderAPIView.as_view(), name='order-create'),
+    path('orders/<int:pk>/delete/', DeleteOrderAPIView.as_view(), name='order-delete'),
 ]
--- cafe_management/api/views.py
+++ cafe_management/api/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import OrderSerializer
+from orders.models import Order
+
 
 class CreateOrderAPIView(APIView):
     def post(self, request, format=None):
@@ -10,3 +12,17 @@ class CreateOrderAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+class DeleteOrderAPIView(APIView):
+    def delete(self, request, pk, format=None):
+        try:
+            order = Order.objects.get(pk=pk)
+            order.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Order.DoesNotExist:
+            return Response({'error': 'Заказ не найден'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
